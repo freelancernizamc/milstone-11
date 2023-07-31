68a7899b90114264b43faab400ed01530dc96ee3
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-08-01 01:34:45 +0600

    module70 banner added

--- src/components/Module70.jsx
+++ src/components/Module70.jsx
@@ -3,7 +3,10 @@
 const Module70 = () => {
     return (
         <div>
-
+            <div>
+                <h1>Welcome to Milstone 11:Module 70</h1>
+            </div>
+            <div></div>
         </div>
     );
 };
